baa2f5b7da4f210a0ab6c7c4f3792d8c1de63159
Asadbek <asadbekasadbek@gmail.com>
2021-03-31 00:22:41 -0400

    reverted watch-anime route

--- app/routes/watch-anime.routes.js
+++ app/routes/watch-anime.routes.js
@@ -1,12 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const router = express.Router();
 const Gapi = require('animet-gogoanime');
 const rateLimit = require("express-rate-limit");
+const request = require('request');
+var rp = require('request-promise');
+const util = require('util');
 const animeLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 1 minutes
     max: 200
 });
 
+const streamApiURL = process.env.LOCAL_ANIMET_STREAM_API_URL;
 
 router.get(
     '/get-anime-available', 
@@ -48,6 +53,57 @@ router.get(
         console.log(error);
     }
 });
+
+/* router.get(
+    '/get-anime-available', 
+    animeLimiter,
+    async(req, res) => {
+    try {
+        var result = [];
+        var animeTitle = String(req.query.animeTitle);
+        var dubTitle = animeTitle + ' ' + '(Dub)';
+        
+        
+        var options = {
+            'method': 'GET',
+            'url': `${streamApiURL}api/search/?word=${animeTitle}&page=1`,
+            'headers': {
+            }
+        };
+        rp(options, function (error, response) {
+            if (error) throw new Error(error);
+            
+            var apiResult = JSON.parse(response.body).results;
+
+            for (let el = 0; el < apiResult.length; el++) {
+                if(result.length > 2) {
+                    break;
+                }
+               const title = apiResult[el].title;
+               if (title && result.length < 2) {
+                   // find SUB 
+                   if (title === animeTitle) {
+                       result.unshift(apiResult[el]);
+                    }
+                    
+                    // find DUB
+                    if (title === dubTitle) {
+                        result.push(apiResult[el]);
+                    }   
+                }   
+            }
+            
+            console.log(result);
+            res.json(result);
+
+        });
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}); */
 
 /* BACKUP: iframe player links */
 router.get('/get-episode-stream', async(req, res) => {
