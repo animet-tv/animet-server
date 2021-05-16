8510cabb73eed48b2db3240591d0c1529584a7bf
Asadbek <asadbekasadbek@gmail.com>
2021-05-16 18:45:27 -0400

    redis to go

--- app/models/genres.model.js
+++ app/models/genres.model.js
@@ -1,8 +1,14 @@
-const redis = require("redis");
-const client = redis.createClient(process.env.REDISCLOUD_URL);
 const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 
+if (process.env.REDISTOGO_URL) {
+    // TODO: redistogo connection
+    var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+} else {
+    var redis = require("redis").createClient();
+}
 
 
 const Anime = mongoose.Schema({
@@ -62,7 +68,7 @@ const Genre = module.exports = mongoose.model('Genre', GenreSchema);
 
 module.exports.getAnimeGenres = async (callback) => {
     try {
-        client.get('GENRES', (err, result) => {
+        redis.get('GENRES', (err, result) => {
                 if (result) {
                     const resultJSON = JSON.parse(result);
                     callback(null, resultJSON);
