3bab3c402d1cddbcfa2a507ee29500e05ab0028c
asadbek064 <asadbekasadbek@gmail.com>
2022-05-11 02:12:03 -0400

    more nsfw

--- app/routes/watch-anime.routes.js
+++ app/routes/watch-anime.routes.js
@@ -46,6 +46,7 @@ const rezeros2 = require(`../../anime60/rezero-s2.json`);
 const tamako_love_story = require(`../../anime60/tamako_love_story.json`);
 const fate_stay_night_movie_1 = require(`../../anime60/fate-stay-night-movie-1.json`);
 const torokase_orgasm = require(`../../anime60/torokase_orgasm.json`);
+const kimi_ga_suki_1 = require(`../../anime60/kimi_ga_suki_1.json`);
 
 const animeLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
@@ -309,6 +310,8 @@ router.get(
                 res.json(fate_stay_night_movie_1);
             } else if (title === `Torokase Orgasm`) {
                 res.json(torokase_orgasm);
+            } else if (title === `Kimi ga Suki 1`) {
+                res.json(kimi_ga_suki_1);
             }
          } catch (error) {
              console.log(error);
