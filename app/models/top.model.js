54e5c070bdfc4e9f6f22f055fa16d8d1178b3051
asadbek064 <asadbekasadbek@gmail.com>
2022-08-09 23:20:24 -0400

    redis ot go gone

--- app/models/top.model.js
+++ app/models/top.model.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 const beautifyUnique = require('mongoose-beautiful-unique-validation');
 
+
+if (process.env.REDISCLOUD_URL) {
+    // rediscloud connection
+    var redis = require('redis');
+    var client = redis.createClient(process.env.REDISCLOUD_URL, {no_ready_check: true});
+
+} else {
+    var client = require("redis").createClient();
+}
+
+/* 
 if (process.env.REDISTOGO_URL) {
     // TODO: redistogo connection
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
@@ -8,7 +19,7 @@ if (process.env.REDISTOGO_URL) {
     client.auth(rtg.auth.split(":")[1]);
 } else {
     var client = require("redis").createClient();
-}
+} */
 
 
 const Anime = mongoose.Schema({
